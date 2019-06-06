aac86cf105b50d5390e2651e807193a39ea6e69e
angel-torres <alas4224@gmail.com>
2019-06-06 17:50:55 -0500

    setup router on server rebuild

---
+++ rebuilt-api/index.js
@@ -0,0 +1,65 @@
+const http = require('http');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+
+const port = 5050;
+
+
+const server = http.createServer(function(req, res) {
+    // Parse URL
+    const parsedUrl = url.parse(req.url, true);
+    const trimmedPath = parsedUrl.pathname.replace(/^\/+|\/+$/g, '');
+    // Parse method
+
+    const method = req.method.toLowerCase();
+    // Parse headers
+    const headers = req.headers;
+    // Parse body
+
+    const decoder = new StringDecoder('utf-8');
+
+    var buffer = '';
+
+    req.on('data', function(data) {
+        buffer += decoder.write(data);
+    });
+
+    req.on('end', function() {
+        buffer += decoder.end();
+
+    const chosenHandler = typeof(router[buffer]) !== 'undefined' ? router[buffer] : router.default;
+
+    chosenHandler(data, function(statusCode, payload) {
+
+        payload = typeof(payload) === 'object' ? payload : {};
+        statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+        const payloadString = JSON.stringify(payload);
+
+        res.setHeader('Content-Type', 'application/json');
+
+        res.writeHead(statusCode);
+
+        res.end(payloadString);
+    });
+
+    });
+
+});
+
+server.listen(port, function(req, res) {
+    console.log(`Listening on port ${port}`);
+})
+
+const handlers = {
+    user: function(data, callback) {
+        callback(200);
+    },
+    default: function(data, callback) {
+        callback(404);
+    }
+}
+
+const router = {
+    'user': handlers.user
+}
